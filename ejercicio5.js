00717c7241330d628671479c8df07319351bb4d6
Ivaan3 <106582115+Ivaan3@users.noreply.github.com>
2022-08-16 20:58:47 +0200

    Add files via upload

---
+++ ejercicio5.js
@@ -0,0 +1,15 @@
+let altura = 174;
+let altura2 = 1.736;
+
+let peso = 71.345
+
+let altura3 = altura2.toFixed(2)
+console.log(altura3)
+
+let peso2 = peso.toPrecision(4)
+console.log(peso2)
+
+let max_valor = Number.MAX_VALUE
+
+
+console.log(max_valor + 1)
