85ba1db7ec88860f1660d1c45a16cd12588a85f4
Ivaan3 <106582115+Ivaan3@users.noreply.github.com>
2022-08-16 21:05:52 +0200

    Add files via upload

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,15 +1,16 @@
 let altura = 174;
-let altura2 = 1.736;
+let altura2 = 1.73;
 
-let peso = 71.345
+let peso = 71.3
 
-let altura3 = altura2.toFixed(2)
+let altura3 = Math.ceil(altura2)
 console.log(altura3)
 
-let peso2 = peso.toPrecision(4)
+let peso2 = Math.floor(peso)
 console.log(peso2)
 
 let max_valor = Number.MAX_VALUE
 
 
 console.log(max_valor + 1)
+console.log(max_valor === Number.MAX_VALUE + 1)
